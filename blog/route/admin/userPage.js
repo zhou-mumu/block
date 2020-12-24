0f4d4089d57409a88418e62308037098327ee8ce
zhou-mumu <953474446@qq.com>
2020-12-24 16:08:48 +0800

    新增用户信息显示分页功能

--- blog/route/admin/userPage.js
+++ blog/route/admin/userPage.js
@@ -2,10 +2,24 @@
 const {User} = require('../../model/user');
 const user = require('../../model/user');
 module.exports = async (req,res) => {
+    //接受客户端传递过来的当前页参数
+    let page = req.query.page || 1;
+    //每一页显示的数据条数
+    let pagesize = 10;
+    //查询用户数据的总数
+    let count = await User.countDocuments({});
+    //总页数
+    let total = Math.ceil(count /pagesize);
+
+    //页码对应的数据查询开始位置
+    let start = (page - 1) * pagesize;
+
     // 将用户信息从数据库中查询出来
-    let users = await User.find({});
+    let users = await User.find({}).limit(pagesize).skip(start);
     // 渲染用户列表模块
     res.render('admin/user',{
-        users: users
+        users: users,
+        page: page,
+        total: total
     });
 }
