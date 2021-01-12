8f7223e90dfc48594183eceb844de43a50afbbe0
zhou-mumu <953474446@qq.com>
2021-01-12 18:01:33 +0800

    分开生产环境和开发环境

--- blog/app.js
+++ blog/app.js
@@ -6,6 +6,10 @@ const path = require('path');
 const bodyPaser = require('body-parser');
 //导入express-session模块
 const session = require('express-session');
+//导入art-template模板引擎
+const template = require('art-template');
+//导入dateFormat第三方模块
+const dateFormat = require('dateformat');
 //创建网站服务器
 const app = express();
 //数据库连接
@@ -26,6 +30,8 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','art');
 //当渲染的后缀为art的模板时 所使用的的模板引擎是什么
 app.engine('art',require('express-art-template'));
+//向模板内部导入dateFormat变量
+template.defaults.imports.dateFormat = dateFormat;
 
 //开放静态资源文件
 app.use(express.static(path.join(__dirname,'public')));
--- blog/route/admin/article-add.js
+++ blog/route/admin/article-add.js
@@ -2,6 +2,8 @@
 const formidable = require('formidable');
 
 const path = require('path');
+
+const { Article } = require('../../model/article');
 module.exports = (req, res) => {
 	// 1.创建表单解析对象
 	const form = new formidable.IncomingForm();
@@ -10,11 +12,21 @@ module.exports = (req, res) => {
 	// 3.保留上传文件的后缀
 	form.keepExtensions = true;
 	// 4.解析表单
-	form.parse(req, (err, fields, files) => {
+	form.parse(req, async(err, fields, files) => {
 		// 1.err错误对象 如果表单解析失败 err里面存储错误信息 如果表单解析成功 err将会是null
 		// 2.fields 对象类型 保存普通表单数据
 		// 3.files 对象类型 保存了和上传文件相关的数据
-		res.send(files);
+		// console.log(files.cover.path.split('public')[1]);
+		// res.send(files);
+		await Article.create({
+			title: fields.title,
+			author: fields.author,
+			publishDate: fields.publishDate,
+			cover: files.cover.path.split('public')[1],
+			content: fields.content
+		})
+        //将页面重定向到文章列表页面
+		res.redirect('/admin/article');
 	})
 	// res.send('ok');
 }
